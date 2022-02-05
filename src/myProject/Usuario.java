a634489e410f48eade49598cb61fa046c36b3f24
Sheilly Ortega 2040051-3743 <sheilly.ortega@correounivalle.edu.co>
2022-02-05 17:31:21 -0500

    Updated Usuario class 0.1

--- src/myProject/Usuario.java
+++ src/myProject/Usuario.java
@@ -1,4 +1,37 @@
 package myProject;
 
+import java.util.ArrayList;
+
 public class Usuario {
+    private ArrayList<String> usuario = new ArrayList<String>();
+    private ArrayList<String> usuarioRegistrado = new ArrayList<String>();
+    private int unUsuario, nivelUsuario;
+    private String usuarioIngresado;
+
+    public Usuario() {
+
+        FileManager fileManager = new FileManager();
+        usuario = fileManager.lecturaUsuario();
+        usuarioRegistrado = fileManager.escribirUsuario(String usuarioIngresado);
+
+        unUsuario = 0;
+        nivelUsuario = 0;
+        usuarioIngresado = " ";
+
+    }
+
+    public int getNivelUsuario(String usuarioIngresado) {
+        for (unUsuario = 0; unUsuario < usuario.size(); unUsuario++) {
+            if (usuario.get(unUsuario) == usuarioIngresado) {
+                nivelUsuario = Integer.parseInt(usuario.get(unUsuario+1));
+            } else {
+                nivelUsuario = 1;
+            }
+        }
+        return nivelUsuario;
+    }
+
+    public void registrarUsuario(String usuarioIngresado){
+        usuarioRegistrado;
+    }
 }
